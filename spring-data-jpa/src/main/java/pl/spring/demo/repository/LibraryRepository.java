69c831c62d92967b9e77d3e45e89329c4cd2534a
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-14 09:07:49 +0100

    Create library repository. Create method findLibraryByName

---
+++ spring-data-jpa/src/main/java/pl/spring/demo/repository/LibraryRepository.java
@@ -0,0 +1,15 @@
+package pl.spring.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import pl.spring.demo.entity.LibraryEntity;
+
+public interface LibraryRepository extends JpaRepository<LibraryEntity, Long>,QueryDslPredicateExecutor<LibraryEntity> {
+	@Query("from LibraryEntity l where upper(l.name) like concat(upper(:name), '%')")
+	List<LibraryEntity> findLibraryByName(@Param("name") String name);
+}
