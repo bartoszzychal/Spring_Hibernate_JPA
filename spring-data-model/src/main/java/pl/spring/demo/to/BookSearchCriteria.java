0491d681b10d7236b3a1e83f109edf4be193ce7d
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-14 09:00:22 +0100

    Create object to find books by criteria

---
+++ spring-data-model/src/main/java/pl/spring/demo/to/BookSearchCriteria.java
@@ -0,0 +1,33 @@
+package pl.spring.demo.to;
+//title, author, libraryName. 
+public class BookSearchCriteria {
+	
+	private String title;
+	private String author;
+	private String libraryName;
+	
+	public BookSearchCriteria(String title, String author, String libraryName) {
+		this.title = title;
+		this.author = author;
+		this.libraryName = libraryName;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getLibraryName() {
+		return libraryName;
+	}
+	public void setLibraryName(String libraryName) {
+		this.libraryName = libraryName;
+	}
+	
+}
