921885e89f70aee862e362aa0e89fcbb657965fc
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-15 13:07:17 +0100

    Added methods from BookSearchCriteriaSerivce.

--- spring-data-jpa/src/main/java/pl/spring/demo/service/BookService.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/BookService.java
@@ -2,6 +2,7 @@ package pl.spring.demo.service;
 
 import java.util.List;
 
+import pl.spring.demo.criteria.BookSearchCriteria;
 import pl.spring.demo.to.BookTo;
 
 public interface BookService {
@@ -9,6 +10,8 @@ public interface BookService {
     List<BookTo> findAllBooks();
     List<BookTo> findBooksByTitle(String title);
     List<BookTo> findBooksByAuthor(String author);
-
+    List<BookTo> findBookByCriteria(BookSearchCriteria bookSearchCriteria);
+	List<BookTo> findBookByCriteriaRepo(BookSearchCriteria bookSearchCriteria);
+	
     BookTo saveBook(BookTo book);
 }
