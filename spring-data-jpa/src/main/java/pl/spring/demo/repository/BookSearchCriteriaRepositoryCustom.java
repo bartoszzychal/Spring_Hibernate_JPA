1c4bfa12fc2fde445a7f67dfb0c91fdaf45eb3d0
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-15 11:58:55 +0100

    Create custom interface to findBookByCriteria in repository.

---
+++ spring-data-jpa/src/main/java/pl/spring/demo/repository/BookSearchCriteriaRepositoryCustom.java
@@ -0,0 +1,10 @@
+package pl.spring.demo.repository;
+
+import java.util.List;
+
+import pl.spring.demo.criteria.BookSearchCriteria;
+import pl.spring.demo.entity.BookEntity;
+
+public interface BookSearchCriteriaRepositoryCustom {
+	List<BookEntity> findBookByCriteria(BookSearchCriteria bookSearchCriteria);
+}
