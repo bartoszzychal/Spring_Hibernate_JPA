e4d12c5e2d262e8cdd095347e5f620407d5c2f74
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-15 13:08:02 +0100

    Moved findBookByCriteria from custom serivce to bookservice

--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
@@ -1,14 +1,18 @@
 package pl.spring.demo.service.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import pl.spring.demo.criteria.BookSearchCriteria;
 import pl.spring.demo.entity.BookEntity;
 import pl.spring.demo.mapper.BookMapper;
 import pl.spring.demo.repository.BookRepository;
+import pl.spring.demo.repository.predicates.BookPredicate;
 import pl.spring.demo.service.BookService;
 import pl.spring.demo.to.BookTo;
 
@@ -41,4 +45,20 @@ public class BookServiceImpl implements BookService {
         entity = bookRepository.save(entity);
         return BookMapper.map(entity);
     }
+    
+	@Override
+	public List<BookTo> findBookByCriteria(BookSearchCriteria bookSearchCriteria) {
+		return BookMapper.map2To(
+				StreamSupport.stream(
+					bookRepository
+					.findAll(BookPredicate.preparePredicate(bookSearchCriteria))
+					.spliterator(), false)
+					.collect(Collectors.toList())
+				);
+	}
+
+	@Override
+	public List<BookTo> findBookByCriteriaRepo(BookSearchCriteria bookSearchCriteria) {
+		return BookMapper.map2To(bookRepository.findBookByCriteria(bookSearchCriteria));
+	}
 }
