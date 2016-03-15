8a41796756c583ced018f525e3cd83c9fc1d44c1
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-15 11:59:48 +0100

    Added second method working on repository custom implementation.

--- spring-data-jpa/src/main/java/pl/spring/demo/service/BookSearchCriteriaService.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/BookSearchCriteriaService.java
@@ -2,9 +2,10 @@ package pl.spring.demo.service;
 
 import java.util.List;
 
-import pl.spring.demo.to.BookSearchCriteria;
+import pl.spring.demo.criteria.BookSearchCriteria;
 import pl.spring.demo.to.BookTo;
 
 public interface BookSearchCriteriaService {
 	List<BookTo> findBookByCriteria(BookSearchCriteria bookSearchCriteria);
+	List<BookTo> findBookByCriteriaRepo(BookSearchCriteria bookSearchCriteria);
 }
