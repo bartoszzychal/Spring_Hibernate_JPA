b31bb5da9eb161e8175aead17dd78b3d5fcf59ba
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-15 12:00:52 +0100

    Implement methods for findBookByCriteria by QueryDsl interface and by custom repository interface.

--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookSearchCriteriaServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookSearchCriteriaServiceImpl.java
@@ -2,14 +2,18 @@ package pl.spring.demo.service.impl;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import pl.spring.demo.repository.LibraryRepository;
+import pl.spring.demo.criteria.BookSearchCriteria;
+import pl.spring.demo.mapper.BookMapper;
+import pl.spring.demo.repository.BookRepository;
+import pl.spring.demo.repository.predicates.BookPredicate;
 import pl.spring.demo.service.BookSearchCriteriaService;
-import pl.spring.demo.to.BookSearchCriteria;
 import pl.spring.demo.to.BookTo;
 
 @Service
@@ -17,10 +21,22 @@ import pl.spring.demo.to.BookTo;
 public class BookSearchCriteriaServiceImpl implements BookSearchCriteriaService {
 
 	@Autowired
-	private LibraryRepository libraryRepository;
+	private BookRepository bookRepository;
 	
 	@Override
 	public List<BookTo> findBookByCriteria(BookSearchCriteria bookSearchCriteria) {
-		return null;
+		return BookMapper.map2To(
+				StreamSupport.stream(
+					bookRepository
+					.findAll(BookPredicate.preparePredicate(bookSearchCriteria))
+					.spliterator(), false)
+					.collect(Collectors.toList())
+				);
 	}
+
+	@Override
+	public List<BookTo> findBookByCriteriaRepo(BookSearchCriteria bookSearchCriteria) {
+		return BookMapper.map2To(bookRepository.findBookByCriteria(bookSearchCriteria));
+	}
+	
 }
