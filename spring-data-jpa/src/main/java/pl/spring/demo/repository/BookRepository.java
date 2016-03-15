e887a3c6ed19ab760758d2e8936a0122476aff7f
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-15 11:57:01 +0100

    Added extends interface for querydsl and custom repository.

--- spring-data-jpa/src/main/java/pl/spring/demo/repository/BookRepository.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/repository/BookRepository.java
@@ -1,13 +1,15 @@
 package pl.spring.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import pl.spring.demo.entity.BookEntity;
-
 import java.util.List;
 
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import pl.spring.demo.entity.BookEntity;
+
+public interface BookRepository extends JpaRepository<BookEntity, Long>,QueryDslPredicateExecutor<BookEntity>, BookSearchCriteriaRepositoryCustom{
 
     @Query("select book from BookEntity book where upper(book.title) like concat(upper(:title), '%')")
     public List<BookEntity> findBookByTitle(@Param("title") String title);
