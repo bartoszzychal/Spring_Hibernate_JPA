370f292418aeffc45c0aa2cd5158d222fd1c1523
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-15 12:05:12 +0100

    Add QueryDslPredicate interface.

--- spring-data-jpa/src/main/java/pl/spring/demo/repository/LibraryRepository.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/repository/LibraryRepository.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import pl.spring.demo.entity.LibraryEntity;
 
-public interface LibraryRepository extends JpaRepository<LibraryEntity, Long>,QueryDslPredicateExecutor<LibraryEntity> {
+public interface LibraryRepository extends JpaRepository<LibraryEntity, Long> {
+	
 	@Query("from LibraryEntity l where upper(l.name) like concat(upper(:name), '%')")
 	List<LibraryEntity> findLibraryByName(@Param("name") String name);
 }
