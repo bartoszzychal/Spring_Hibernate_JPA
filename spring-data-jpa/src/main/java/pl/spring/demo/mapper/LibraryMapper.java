d41298c80527e85f89b2321e678f271e8ff569ad
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-15 12:04:41 +0100

    add map2Name method.

--- spring-data-jpa/src/main/java/pl/spring/demo/mapper/LibraryMapper.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/mapper/LibraryMapper.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import pl.spring.demo.entity.BookEntity;
 import pl.spring.demo.entity.LibraryEntity;
@@ -11,31 +12,39 @@ import pl.spring.demo.to.BookTo;
 import pl.spring.demo.to.LibraryTo;
 
 public class LibraryMapper {
-	
-	public static LibraryTo map (LibraryEntity libraryEntity){
-		if(libraryEntity != null){
+
+	public static LibraryTo map(LibraryEntity libraryEntity) {
+		if (libraryEntity != null) {
 			return new LibraryTo(libraryEntity.getId(), libraryEntity.getName(), mapBooks(libraryEntity.getBooks()));
 		}
 		return null;
 	}
-	
-	public static LibraryEntity map(LibraryTo libraryTo){
-		if(libraryTo != null){
+
+	public static LibraryEntity map(LibraryTo libraryTo) {
+		if (libraryTo != null) {
 			return new LibraryEntity(libraryTo.getId(), libraryTo.getName());
 		}
 		return null;
 	}
-	
-	public static List<LibraryTo> map2Entity(List<LibraryEntity> libraryEntities){
-		return	libraryEntities.stream().map(LibraryMapper::map).collect(Collectors.toList());
+
+	public static List<LibraryTo> map2Entity(Iterable<LibraryEntity> libraryEntities) {
+		return StreamSupport.stream(libraryEntities.spliterator(), false)
+				.map(LibraryMapper::map)
+				.collect(Collectors.toList());
+	}
+
+	public static List<LibraryEntity> map2To(List<LibraryTo> libraryTos) {
+		return libraryTos.stream()
+				.map(LibraryMapper::map)
+				.collect(Collectors.toList());
 	}
 	
-	public static List<LibraryEntity> map2To(List<LibraryTo> libraryTos){
-		return	libraryTos.stream().map(LibraryMapper::map).collect(Collectors.toList());
+	public static String map2Name(LibraryEntity libraryEntity){
+		return libraryEntity.getName();
 	}
 
 	private static Set<BookTo> mapBooks(Collection<BookEntity> books) {
-		if(books != null){
+		if (books != null) {
 			return BookMapper.map2To(books.stream().collect(Collectors.toList())).stream().collect(Collectors.toSet());
 		}
 		return null;
