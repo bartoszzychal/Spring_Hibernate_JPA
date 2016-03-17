203afa1f6becb8fe2b97899f725e5e352d51078c
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-17 11:20:02 +0100

    Added new test

--- spring-data-jpa/src/test/java/pl/spring/demo/dao/BookDaoImplTest.java
+++ spring-data-jpa/src/test/java/pl/spring/demo/dao/BookDaoImplTest.java
@@ -3,6 +3,7 @@ package pl.spring.demo.dao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.AssertThrows;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import pl.spring.demo.entity.BookEntity;
@@ -20,6 +21,14 @@ public class BookDaoImplTest {
     @Autowired
     private BookDao bookDao;
 
+    @Test
+    public void testShouldFindAllBooks() {
+    	// when
+    	List<BookEntity> findAll = bookDao.findAll();
+    	// then
+    	assertNotNull(findAll);
+    	assertFalse(findAll.isEmpty());
+    }
     @Test
     public void testShouldFindBookById() {
         // given
